e997c3e68ae2dc0d8eb9fe90e9f4379ac49df3e5
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-08-02 20:43:06 -0300

    Add files via upload

---
+++ Python/Exercicios/ex05.py
@@ -0,0 +1,9 @@
+n = int(input('Digite um número: '))
+a = n - 1
+s = n + 1
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, a, s))
+
+#Também da pra utilizar sem as variaveis utilizando so o format mas se o código for maior tera complicações.
+
+#Ex: n = int(input('Digite um número: '))
+#print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, (n-1), (n+1)))
