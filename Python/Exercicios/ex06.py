fffd7cdb7633eaacb705107be7d4cd6079cc9ebf
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-08-03 14:28:57 -0300

    Add files via upload

---
+++ Python/Exercicios/ex06.py
@@ -0,0 +1,9 @@
+n = int(input('Digite um número: '))
+a = n * 2
+b = n * 3
+c = n ** (1/2)
+
+print('O dobro de {} vale {}.'.format(n, a))
+print('O triplo de {} vale {}. \nA raiz quadrada de {} é igual a {:2f}.'.format(n, b, n, c))
+
+#Também da pra utilizar as bariaveis dentro do format.
