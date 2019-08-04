55f9eea3503306742c5de686315fa280587390ca
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-08-04 13:37:27 -0300

    Update ex011.py

--- Python/Exercicios/ex011.py
+++ Python/Exercicios/ex011.py
@@ -1,1 +1,6 @@
-print('ERROR 404')
+larg = float(input('Digite a largura da sua parede: '))
+alt = float(input('Digite a altura da sua parede: '))
+area = larg * alt
+print('Sua parede tem a dimensão de {}x{} e sua área é de {}m²'.format(larg, alt, area))
+tinta = area / 2
+print('Para pintar essa parede, você precisará de {}l de tinta.'.format(tinta))
