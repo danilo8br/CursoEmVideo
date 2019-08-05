10f3fc633a48cd90adc37d1672ee3178a4e0050a
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-08-05 10:02:51 -0300

    Update ex013.py

--- Python/Exercicios/ex013.py
+++ Python/Exercicios/ex013.py
@@ -1,1 +1,3 @@
-print('ERROR 404')
+salario = float(input('Qual e o salario do funcionáro? '))
+aumento =  salario + (salario * 15 / 100)
+print('Um funcionario ganhava {}R$, com 15% de aumento, passa a receber {:.2f}'.format(salario, aumento))
