9f85f479315e43569600626f6bb9c8c13f949bbd
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-08-18 20:10:28 -0300

    Update ex017.py

--- Python/Exercicios/ex017.py
+++ Python/Exercicios/ex017.py
@@ -1,1 +1,4 @@
-print('ERROR 404')
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente:'))
+hi = math.hypot(co, ca)
+print('A hipotenusa vai medir {:.2f}'.format(hi))
