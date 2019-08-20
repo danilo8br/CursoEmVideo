2195c05f59e9a039a29f3a68b33baf2f35f6b323
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-08-20 19:35:19 -0300

    Update ex022.py

--- Python/Exercicios/ex022.py
+++ Python/Exercicios/ex022.py
@@ -1,1 +1,11 @@
-print('ERROR 404')
+nome = str(input('Digite seu nome: ')).strip()
+print('Analisando seu nome')
+print('Seu nome em maiusculas é {}'.format(nome.upper()))
+print('Seu nome em minusculas é {}'.format(nome.lower()))
+print('Seu nome tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
+print('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
+
+#Tambem da pra utilziar de forma mais simples escolhendo o nome por exemplo:
+
+#separa = nome.split()
+#print('Seu primeiro nome é {} e ele tem {} letras'.format(separa[0], len(separa[0])
