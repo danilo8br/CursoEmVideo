e47a4d4da3fd742d02d90b83673dce8fca3b2aab
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-08-21 12:25:17 -0300

    Update ex026.py

--- Python/Exercicios/ex026.py
+++ Python/Exercicios/ex026.py
@@ -1,1 +1,4 @@
-print('ERROR 404')
+frase = str(input('Digite uma frase: ')).upper().strip()
+print('A letra A aparece {} vezes na frase'.format(frase.count('A')))
+print('A primeira letra A apareceu na posição {}'.format(frase.find('A')+1))
+print('A ultima letra A apareceu na posição {}'.format(frase.rfind('A)+1))
