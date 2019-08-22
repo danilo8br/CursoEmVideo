788e70759d0d233e461c01e2e31bcb1cc11410a5
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-08-22 18:09:00 -0300

    Update ex028.py

--- Python/Exercicios/ex028.py
+++ Python/Exercicios/ex028.py
@@ -1,1 +1,13 @@
-print('ERROR 404')
+from random import randint
+from time import sleep
+computador = randint(0, 5) # Faz o computador "Pensar"
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('-=-')
+jogador = int(input('Em que numero eu pensei? '))
+print('Processando...')
+sleep(3)
+if jogador == computador:
+  print('PARABÉNS, você conseguiu me vencer!')
+else:
+  print(GANHEI! Eu pensei no número {}!.format(computador, jogador)
