f5e3d976851444e1d9ef927e772dbe52a49fb39d
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-08-24 13:07:42 -0300

    Update ex029.py

--- Python/Exercicios/ex029.py
+++ Python/Exercicios/ex029.py
@@ -1,1 +1,7 @@
-print('ERROR 404')
+velocidade = float(input('Qual a velocidade atual do seu carro? '))
+if velocidade > 80:
+  print('MULTADO! Você excedeu o limite permitido que é de 80Km/h')
+  multa = (velocidade-80) * 7
+  print('Você deve pagar a multa de R${:.2F)'.format(multa))
+print('Tenha um bom dia! Dirija com segurança')
+                   
