3d86ffc6f4fc1c3f9c794ebcefc97c3bcc7cd040
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-08-26 09:43:50 -0300

    Update ex033.py

--- Python/Exercicios/ex033.py
+++ Python/Exercicios/ex033.py
@@ -1,1 +1,17 @@
-print('ERROR 404')
+a = int(input('Pimeiro valor: '))
+b = int(input('Segundo valor: '))
+c = int(input('Terceiro valor: '))
+#Verificando quem é menor
+menor = a
+if b < a and b < c:
+  menor = b
+if c < a and c < b:
+  menor = c
+#Verificando quem é maior
+maior = a
+if b > a and b > c:
+  maior = b
+if c > a and c > b:
+  maior = c
+print('O maior valor digitado é {}'.format(maior))
+print('O menor  valor digitado é {}'.format(menor))
