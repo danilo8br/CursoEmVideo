1653edfe04562acf4949a4cdf9c6767fbbc07fb4
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-09-02 09:56:34 -0300

    Update ex039.py

--- Python/Exercicios/ex039.py
+++ Python/Exercicios/ex039.py
@@ -8,18 +8,20 @@ print('Faça um programa que leia o ano de nascimento de uma jovem e informe,\n'
 print(30*'-=-')
 print('\n')
 
+atual = date.today().year
 nasc = int(input('Ano de nascimento: '))
-ano = date.today().year
-idade = ano - nasc
-
-
-print('Quem nasceu em {} tem {} anos em {}'.format(nasc, idade, ano))
-
-if idade < 18:
-    f = 18 - idade
-    print('Você irá se alistar em {} daqui á {} anos'.format((f + ano), 18 - idade))
-elif idade > 18:
-    f = idade - 18
-    print('Você já se alistou em {}, cerca de {} anos atrás'.format((ano - f),f))
-else:
+idade = atual - nasc
+print('Quem nasceu em {} tem {} anos em {}'.format(nasc, idade, atual))
+if idade == 18:
     print('Você tem que se alistar IMEDIATAMENTE!')
+elif idade < 18:
+    saldo = idade - 18
+    print('Ainda faltam {} anos para o alistamento'.format(saldo))
+    ano = atual + saldo
+    print('Seu alistamento será em {}'.format(ano))
+elif idade > 18:
+    saldo = 18 - idade
+    print('Você ja deveria ter se alistado a {} anos'.format(saldo))
+    ano = atual - saldo
+    print('Seu alistamento foi em {}'.format(ano))
+    
