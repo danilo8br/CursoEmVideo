5cd1894151c1939d5c518221a4d2b96248261b9b
danilo8br <51414398+danilo8br@users.noreply.github.com>
2020-01-22 22:56:10 -0300

    Update ex060.py

--- Python/Exercicios/ex060.py
+++ Python/Exercicios/ex060.py
@@ -1,1 +1,17 @@
-print('ERROR 404')
+# Jeito simples
+"""from math import factorial
+n = int(input("Digite um numero para calcular seu fatorial: "))
+f = factorial(n)
+print(f"O fatorial de {n} é {f}")
+"""
+# Utilizando com while
+n = int(input("Digite um numero para calcular seu fatorial: "))
+c = n
+f = 1
+print(f"Calculando {n}! = ", end="")
+while c > 0:
+    print(f"{c} ", end ='')
+    print(" x " if c > 1 else " = ", end="")
+    f *= c
+    c -= 1
+print(f"{f}")
