221f046015b0ebf11e60e2d1b84731c2f4f40f87
danilo8br <51414398+danilo8br@users.noreply.github.com>
2020-01-24 10:43:03 -0300

    Update ex065.py

--- Python/Exercicios/ex065.py
+++ Python/Exercicios/ex065.py
@@ -1,11 +1,11 @@
 resp = "S"
-soma = quant = media = 0
+soma = quant = media = maior = menor = 0
 while resp in "Ss":
     num = int(input("Digite um numero: "))
     soma += num
     quant += 1
     if quant == 1:
-        maior = menor = n
+        maior = menor = num
     else:
         if num > maior:
             maior = num
