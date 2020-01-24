796d806b13fc8b9a9febd5fe873ca3589a8502f0
danilo8br <51414398+danilo8br@users.noreply.github.com>
2020-01-24 10:14:26 -0300

    Update ex064.py

--- Python/Exercicios/ex064.py
+++ Python/Exercicios/ex064.py
@@ -1,1 +1,21 @@
-print('ERROR 404')
+# Minha solução
+soma = 0
+cont = 0
+n = 0
+while n != 999:
+    n = int(input("Digite um numero para [999 para parar]: "))
+    if n != 999:
+        cont = cont + 1
+        soma = soma + n
+print(f"Voce digitou {cont} numeros e a soma entre eles foi {soma}")
+
+
+# Solução do guanabara
+
+num = cont = soma = 0
+num = int(input("Digite um numero para [999 para parar]: "))
+while num != 999:
+    soma += num
+    cont += 1
+    num = int(input("Digite um numero [999 para parar]: "))
+print(f"Voce digitou {cont} numeros e a soma entre eles foi {soma}")
