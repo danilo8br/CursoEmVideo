f92e0b4cbcdd22cc653a13d306b0b779b2036b00
danilo8br <51414398+danilo8br@users.noreply.github.com>
2020-01-24 02:20:37 -0300

    Update ex061.py

--- Python/Exercicios/ex061.py
+++ Python/Exercicios/ex061.py
@@ -1,1 +1,9 @@
-print('ERROR 404')
+primeiro = int(input("Primeiro termo: "))
+razao = int(input("Razao: "))
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print(f"{termo} ", end="")
+    termo += razao
+    cont += 1
+print("FIM")
