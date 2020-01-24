551e7cf886457825709fd66f9b15432c07fe8ba6
danilo8br <51414398+danilo8br@users.noreply.github.com>
2020-01-24 10:15:38 -0300

    Update ex062.py

--- Python/Exercicios/ex062.py
+++ Python/Exercicios/ex062.py
@@ -1,1 +1,15 @@
-print('ERROR 404')
+primeiro = int(input("Primeiro termo: "))
+razao = int(input("Razao: "))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+while mais != 0:    
+    total = total + mais
+    while cont <= total:
+        print(f"{termo} ", end="")
+        termo = termo + razao
+        cont = cont + 1
+    print("PAUSE")
+    mais = int(input("Quantos termos voce quer mostrar a mais? "))
+print(f"Progressao finalizada com {total} termos mostrados")
