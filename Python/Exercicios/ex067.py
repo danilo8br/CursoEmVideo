a93725ef27b9d1e7dd00f9fce78014577b506cdc
danilo8br <51414398+danilo8br@users.noreply.github.com>
2020-01-28 03:06:26 -0300

    Update ex067.py

--- Python/Exercicios/ex067.py
+++ Python/Exercicios/ex067.py
@@ -1,1 +1,7 @@
-print('ERROR 404')
+while True:
+  n = int(input("Digite um numero: 
+  if n < 0:
+      break              
+  for a in range(1, 11):
+      print(f"{n} x {a} = {n*a}")
+print("Acabou")
