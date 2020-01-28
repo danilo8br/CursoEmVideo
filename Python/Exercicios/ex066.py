143849e5a1d39830cd9f332cd61bfb526c6e856f
danilo8br <51414398+danilo8br@users.noreply.github.com>
2020-01-28 02:31:47 -0300

    Update ex066.py

--- Python/Exercicios/ex066.py
+++ Python/Exercicios/ex066.py
@@ -1,1 +1,8 @@
-print('ERROR 404')
+cont = soma = 0
+while True:
+  n = int(input("Digite um numero: ")
+  if n == 999:
+    break
+  cont = += 1
+  soma += n
+print(f"O numero de vezes digitados foi {cont} e a soma foi {soma}")
