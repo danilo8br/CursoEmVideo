e01d902668f83b291677c0589a8e06cab41d0cf8
Danilo Donato <51414398+danilo8br@users.noreply.github.com>
2020-01-31 23:39:44 -0300

    Update ex069.py

--- Python/Exercicios/ex069.py
+++ Python/Exercicios/ex069.py
@@ -1,1 +1,22 @@
-print('ERROR 404')
+maior = homens = mulher = 0
+while True:
+    idade = int(input("Idade: "))
+
+    sexo = " "
+    while sexo not in "MmFf":
+        sexo = str(input("Sexo: "))
+    if idade > 18:
+        maior += 1
+    if sexo == "M":
+        homens += 1
+    if sexo == "F" and idade < 20:
+        mulher += 1
+        
+    continuar = " "    
+    while continuar not in "SsNn"    
+        continuar = str(input("Quer continuar? [S/N]")).strip().upper()[0]
+    if continuar == "N":
+        break
+print(f"Total de pessoas maiores de 18 anos: {maior}")
+print(f"Ao todo temos {homens} homens cadastrados")
+print(f"E temos {mulher} com menos de 20 anos")
