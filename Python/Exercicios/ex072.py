bc6cd1c54d860943700483bd54efbbb93f3e61ea
Danilo Donato <51414398+danilo8br@users.noreply.github.com>
2020-02-03 16:34:18 -0300

    Update ex072.py

--- Python/Exercicios/ex072.py
+++ Python/Exercicios/ex072.py
@@ -1,1 +1,11 @@
-print('ERROR 404')
+numeros = ("zero", "um", "dois", "tres", "quatro",
+          "cinco", "seis", "sete", "oito", "nove",
+          "dez", "onze", "doze", "treze" , "cartoze",
+          "quinze", "dezesseis", "dezessete", "dezoito", "dezenove", "vinte")
+
+while True:
+    n = int(input("Digite um numero entre 0 a 20: "))
+    if 0 <= n <= 20:
+        break
+    print("Tente novamente. ", end="") 
+print(f"Voce digitou o numero {numeros[n]}")
