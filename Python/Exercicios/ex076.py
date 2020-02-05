f3c7e63e1cef4285aa84f97baadf1bbab3febb18
Danilo Donato <51414398+danilo8br@users.noreply.github.com>
2020-02-05 14:05:02 -0300

    Update ex076.py

--- Python/Exercicios/ex076.py
+++ Python/Exercicios/ex076.py
@@ -1,1 +1,16 @@
-print('ERROR 404')
+t = ("Lapis", 1.75,
+    "Borracha", 2.00,
+    "Caderno", 15.00,
+    "Transferidor", 25.00,
+    "Compasso", 4.20,
+    "Mochila", 9.99,
+    "Canetas", 22.30,
+    "Livro", 34.90)
+print("="*37)
+print("LISTAGEM DE PREÇOS")
+print("="*37)
+for item in range(0, len(t)):
+    if item % 2 == 0:
+        print(f"{t[item]:.<30}", end="")
+    else:
+        print(f"{t[item]:>5.2f}")
