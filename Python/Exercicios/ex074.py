0bc4b3ee144cdf0179937cf2c1def9cc6c7ca96a
Danilo Donato <51414398+danilo8br@users.noreply.github.com>
2020-02-05 14:04:09 -0300

    Update ex074.py

--- Python/Exercicios/ex074.py
+++ Python/Exercicios/ex074.py
@@ -1,1 +1,10 @@
-print('ERROR 404')
+from random import randint
+
+numeros = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+
+print(f"Os valores sorteados foram: ", end="")
+for n in numeros:
+    print(f"{n}", end=" ")
+    
+print(f"\nO maior valores sorteado foi: {max(numeros)}")
+print(f"O menos valor sorteado foi: {min(numeros)}")
