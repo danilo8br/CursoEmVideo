d971ce844fc81af6aceea8951b417bdf555aff93
Danilo Donato <51414398+danilo8br@users.noreply.github.com>
2020-02-05 14:05:24 -0300

    Update ex077.py

--- Python/Exercicios/ex077.py
+++ Python/Exercicios/ex077.py
@@ -1,1 +1,8 @@
-print('ERROR 404')
+palavras = ("APRENDER", "PROGRAMAR", "LINGUAGEM", "PYTHON",
+          "CURSO", "GRATIS", "ESTUDAR", "PRATICAR",
+          "TRABALHAR", "MERCADO", "PROGRAMADOR", "FUTURO")
+for p in palavras:
+    print(f"\nNa palavra {p} temos ", end="")
+    for letra in p:
+        if letra.upper() in "AEIOU":
+            print(letra, end=" ")
